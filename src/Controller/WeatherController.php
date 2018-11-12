ce3feb6763f0692ece1e0d053c25153ee52d6c35
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2018-11-13 00:08:39 +0200

    Implemented not real api to get weather in controller and print in twig template

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,14 +2,30 @@
 
 namespace App\Controller;
 
+use App\GoogleAPI\WeatherService;
+use App\Model\NullWeather;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WeatherController extends AbstractController
 {
     public function index()
     {
+        try {
+
+            $fromGoogle = new WeatherService();
+            $weather = $fromGoogle->getToday();
+
+        } catch (\Exception $exp) {
+            $weather = new NullWeather();
+        }
+
         return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
+            'weatherData' => [
+                'date'      => $weather->getDate()->format('Y-m-d'),
+                'dayTemp'   => $weather->getDayTemp(),
+                'nightTemp' => $weather->getNightTemp(),
+                'sky'       => $weather->getSky()
+            ],
         ]);
     }
 }
---
+++ src/GoogleApi/WeatherService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GoogleApi;
+
+use App\Model\NullWeather;
+use App\Model\Weather;
+
+class WeatherService
+{
+    /**
+     * @return Weather
+     * @throws \Exception
+     */
+    public function getToday(): Weather
+    {
+        $today = $this->load(new NullWeather());
+        $today->setDate(new \DateTime());
+
+        return $today;
+    }
+
+    /**
+     * @param Weather $before
+     * @return Weather
+     * @throws \Exception
+     */
+    private function load(Weather $before): Weather
+    {
+        $now = new Weather();
+        $base = $before->getDayTemp();
+        $now->setDayTemp(random_int(5 - $base, 5 + $base));
+        $base = $before->getNightTemp();
+        $now->setNightTemp(random_int(-5 - abs($base), -5 + abs($base)));
+        $now->setSky(random_int(1, 3));
+
+        return $now;
+    }
+}
